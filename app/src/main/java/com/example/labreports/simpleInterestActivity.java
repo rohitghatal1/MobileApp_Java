913331f6e8c6de79414db47805c150ad2999999e
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-07-01 18:21:23 +0545

    updated

--- app/src/main/java/com/example/labreports/simpleInterestActivity.java
+++ app/src/main/java/com/example/labreports/simpleInterestActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.simple_interest);
 
         etPrincipal = findViewById(R.id.etPrincipal);
         etRate = findViewById(R.id.etRate);
